39e1e52b590bb2ea45ab71a1141f8703aa4a5349
unknown <smitthapa808@gmail.com>
2026-08-30 02:01:14 +0545

    new

--- config/env.config.js
+++ config/env.config.js
@@ -4,7 +4,7 @@ module.exports = {
   "NODE_ENV": "production",
   "ADMIN_CODE": "9804344434",
   "SESSION_SECRET": "RenderHostedScoreSync_2024_SuperSecureKey_ReplaceInProd123!",
-  "MONGODB_URI": "mongodb+srv://demon:P6whwJ8qsMfIZg2F@cluster0.ix4q7ng.mongodb.net/?appName=Cluster0",
+  "MONGODB_URI": "mongodb+srv://DEMON:1RpRCPfA2TIjcXXL@cluster0.znuinux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   "UPSTASH_REDIS_REST_URL": "https://striking-ocelot-154242.upstash.io",
   "UPSTASH_REDIS_REST_TOKEN": "gQAAAAAAAlqCAAIgcDEyYmZmNTdhMzBkODE0NmMzODYzYjZlOWU2MGM4NGVjYQ",
   "LOG_LEVEL": "info",
