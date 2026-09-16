b9ad6b00ed6ad8d76d9b778126862bebf49f7f82
unknown <smitthapa808@gmail.com>
2026-09-16 10:01:32 +0545

    hello world

--- config/env.config.js
+++ config/env.config.js
@@ -4,9 +4,9 @@ module.exports = {
   "NODE_ENV": "production",
   "ADMIN_CODE": "9804344434",
   "SESSION_SECRET": "RenderHostedScoreSync_2024_SuperSecureKey_ReplaceInProd123!",
-  "MONGODB_URI": "mongodb+srv://shanahitman967_db_user:cwgVxAQloeQmZ4gy@cluster0.iouptmf.mongodb.net/?appName=Cluster0",
-  "UPSTASH_REDIS_REST_URL": "https://dear-polecat-206495.upstash.io",
-  "UPSTASH_REDIS_REST_TOKEN": "gQAAAAAAAyafAAIgcDEzZGJlOTAyODMwODk0NGE3OGZjZmI3OTIwMzYzYTA2OA",
+  "MONGODB_URI": "mongodb+srv://DEMON:1RpRCPfA2TIjcXXL@cluster0.znuinux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  "UPSTASH_REDIS_REST_URL": "https://striking-ocelot-154242.upstash.io",
+  "UPSTASH_REDIS_REST_TOKEN": "gQAAAAAAAlqCAAIgcDEyYmZmNTdhMzBkODE0NmMzODYzYjZlOWU2MGM4NGVjYQ",
   "LOG_LEVEL": "info",
   "LOG_TO_FILE": "true"
 };
